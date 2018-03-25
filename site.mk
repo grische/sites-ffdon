073a0f0dda6de6b45bfcb3b2215a4d42869115b0
ff-mgk <freifunk@growngizmo.de>
2018-03-25 09:57:12 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -1,33 +1,34 @@
 GLUON_SITE_PACKAGES := \
-        ffpb-ebtables-net-rules \
         gluon-alfred \
         gluon-respondd\
-        gluon-autoupdater \
-        gluon-config-mode-autoupdater \
-        gluon-config-mode-contact-info \
-        gluon-config-mode-core \
-        gluon-config-mode-geo-location \
-        gluon-config-mode-hostname \
-        gluon-config-mode-tunneldigger \
-        gluon-ebtables-filter-multicast \
-        gluon-ebtables-filter-ra-dhcp \
-        gluon-luci-admin \
-        gluon-luci-autoupdater \
-        gluon-luci-portconfig \
-        gluon-luci-private-wifi \
-        gluon-luci-wifi-config \
-        gluon-mesh-batman-adv-15 \
-        gluon-mesh-vpn-tunneldigger \
-        gluon-next-node \
-        gluon-radvd \
-        gluon-setup-mode \
-        gluon-status-page \
-        haveged \
-        iptables \
-        gluon-tunneldigger-watchdog \
-        gluon-migrate-vpn \
-        iwinfo
-         
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-hostname \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-ebtables-source-filter \
+	gluon-web-admin \
+	gluon-web-autoupdater \
+	gluon-web-network \
+	gluon-web-private-wifi \
+	gluon-web-wifi-config \
+	gluon-mesh-batman-adv-15 \
+	gluon-mesh-vpn-tunneldigger \
+	gluon-radvd \
+	gluon-setup-mode \
+	gluon-status-page \
+	haveged \
+	iptables \
+	gluon-mesh-vpn-core \
+	iwinfo \
+	gluon-ssid-changer \
+	gluon-config-mode-show-mac \
+	gluon-rfkill-disable
+
 GLUON_PRIORITY ?= 3
 GLUON_LANGS ?= de
+GLUON_ATH10K_MESH := ibss
 GLUON_REGION ?= eu
